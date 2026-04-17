fd1af9cb2cffdc7a80a19e6788d0a17a9fed5b4c
jyjcho <jeremy.cho86@gmail.com>
2026-04-17 09:27:53 -0700

    Initial Commit

---
+++ simplypiano/simplypianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  simplypianoApp.swift
+//  simplypiano
+//
+//  Created by Jeremy Cho on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct simplypianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
